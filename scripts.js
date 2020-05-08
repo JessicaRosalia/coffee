0e2365cafa77716f38f6961300433ad16fc67c21
Rosália Fernandes <jessicarosalia678@gmail.com>
2020-05-08 16:48:59 -0300

    adicionando eventos js, corrigindo tamanho do modal content e corrigindo posição do modal

---
+++ scripts.js
@@ -0,0 +1,12 @@
+const modal = document.querySelector(".modal");
+const cards = document.querySelectorAll(".card");
+
+for(let card of cards){
+	card.addEventListener('click',function(){
+		modal.classList.add('active');
+	});
+}
+
+document.querySelector(".modal-close").addEventListener("click", function(){
+	modal.classList.remove("active");
+});
